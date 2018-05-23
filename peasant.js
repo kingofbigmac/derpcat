bf9f2416038f391d0ce37233b442ee14581b203f
kingofbigmac <kingofbigmac@gmail.com>
2018-05-23 15:23:20 -0500

    Update peasant.js

--- peasant.js
+++ peasant.js
@@ -10,7 +10,7 @@ var peasants = 0;
 function buyPeasant(){
     var peasantCost = Math.floor(100 * Math.pow(1.1,peasants));     //works out the cost of this peasant
     if(cookies >= peasantCost){                                   //checks that the player can afford the peasant
-        peasants = peasants + 1;                                   //increases number of peasants
+        peasants = peasants + 10;                                   //increases number of peasants
     	cookies = cookies - peasantCost;                          //removes the cookies spent
         document.getElementById('peasants').innerHTML = peasants;  //updates the number of peasants for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
