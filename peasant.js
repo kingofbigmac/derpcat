45e88524555efab025226c943032a7846115f091
kingofbigmac <kingofbigmac@gmail.com>
2018-05-23 14:33:03 -0500

    Update peasant.js

--- peasant.js
+++ peasant.js
@@ -13,7 +13,7 @@ function buyPeasant(){
         peasants = peasants + 1;                                   //increases number of peasants
     	peasant = peasant - peasantCost;                          //removes the peasant spent
         document.getElementById('peasants').innerHTML = peasants;  //updates the number of peasants for the user
-        document.getElementById('peasant').innerHTML = cookies;  //updates the number of peasant for the user dont touch
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of peasant for the user dont touch
     };
     var nextCost = Math.floor(100 * Math.pow(1.1,peasants));       //works out the cost of the next cursor
     document.getElementById('peasantCost').innerHTML = nextCost;  //updates the cursor cost for the user
