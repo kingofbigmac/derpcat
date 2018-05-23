2967b3b2340f2dc55ad0140c1880c71a486e2b40
kingofbigmac <kingofbigmac@gmail.com>
2018-05-23 14:10:11 -0500

    Update peasant.js

--- peasant.js
+++ peasant.js
@@ -5,22 +5,22 @@ function cookieClick(number){
     document.getElementById("peasant").innerHTML = peasant;
 };
 
-var cursors = 0;
+var peasants = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(100 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+function buyPeasant(){
+    var cursorCost = Math.floor(100 * Math.pow(1.1,peasants));     //works out the cost of this cursor
     if(peasant >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
+        peasants = peasants + 1;                                   //increases number of peasants
     	peasant = peasant - cursorCost;                          //removes the peasant spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
+        document.getElementById('peasants').innerHTML = peasants;  //updates the number of peasants for the user
         document.getElementById('peasant').innerHTML = peasant;  //updates the number of peasant for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,peasants));       //works out the cost of the next cursor
+    document.getElementById('peasantCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	peasantClick(peasants);
 	
 }, 1000);
